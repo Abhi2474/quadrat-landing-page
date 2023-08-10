f7b5a7de2321685c7ed1b38cecbb0ad8b285abfe
Aman <amangautam2474@gmail.com>
2023-08-10 15:49:35 +0530

    fix: style

--- observer.js
+++ observer.js
@@ -1,13 +1,14 @@
-const images = document.querySelectorAll(".midImg");
+const images = document.querySelectorAll(".hide");
 
 const imageObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) imageObserver.unobserve(entry.target);
     });
   },
   {
-    threshold: 0.2,
+    threshold: 0.5,
   }
 );
 images.forEach((img) => {
